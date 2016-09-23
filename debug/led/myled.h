cc4ff684a390a5b804916cd88e66fb9dd72e2da7
zeroway <M_O_Bz@163.com>
2016-09-23 11:28:37 +0000

    relayout led

---
+++ debug/led/myled.h
@@ -0,0 +1,28 @@
+#ifndef _MYLED_H_
+#define _MYLED_H_
+
+#include <linux/kernel.h>
+#include <linux/hrtimer.h>
+#include <linux/i2c.h>
+#include <linux/input.h>
+#include <linux/module.h>
+#include <linux/delay.h>
+#include <linux/i2c.h>
+#include <linux/proc_fs.h>
+#include <linux/string.h>
+#include <asm/uaccess.h>
+#include <linux/vmalloc.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/gpio.h>
+#include <linux/of.h>
+#include <linux/of_gpio.h>
+#include <linux/of_device.h>
+#include <linux/irq.h>
+#include <linux/regulator/consumer.h>
+#include <linux/miscdevice.h>
+
+#define IOCTL_INT_VALUE		0
+#define IOCTL_STRING_VALUE	1
+
+#endif
