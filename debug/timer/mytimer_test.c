5731367ca22aac1df16373d54b2fd6a1c4459ef8
zeroway <M_O_Bz@163.com>
2016-10-11 14:15:07 +0000

    use timer to do a loop job

--- debug/timer/mytimer_test.c
+++ debug/timer/mytimer_test.c
@@ -11,11 +11,13 @@ struct self_define_struct {
 };
 
 struct self_define_struct g_sds;
+static int g_cnt = 0;
 
 static void timeout_handler(unsigned long tdata)
 {
-	printk("%s, %d\n", __FUNCTION__, __LINE__);
-	del_timer(&g_sds.timer);
+	printk("%s being called %d times\n", __FUNCTION__, g_cnt++);
+	g_sds.timer.expires = jiffies + EXPIRES_PERIOD;
+	add_timer(&g_sds.timer);
 }
 
 static int mytimer_test_init(void)
@@ -32,6 +34,7 @@ static int mytimer_test_init(void)
 static void  mytimer_test_exit(void)
 {
 	printk("%s, %d\n", __FUNCTION__, __LINE__);
+	del_timer(&g_sds.timer);
 }
 
 module_init(mytimer_test_init)
