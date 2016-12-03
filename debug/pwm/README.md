9c20325f07558ba9ef7d0ba20d340252ffc83214
zeroway <M_O_Bz@163.com>
2016-12-03 14:04:42 +0000

    pwm backlight : add dtsi file

--- debug/pwm/README.md
+++ debug/pwm/README.md
@@ -2,6 +2,43 @@
 
 [参考文章http://www.wowotech.net/comm/pwm_overview.html?utm_source=tuicool](http://www.wowotech.net/comm/pwm_overview.html?utm_source=tuicool)
 
+# 硬件连接图
+
+![pwm](./pwm01.png)
+
+![pwm](./pwm02.png)
+
+从下面pwm控制器的设备树描述可以知道GPIO7_A0已经是PWM
+
+	#define PWM0 0x7a01
+	GPIO7_A0 配置成了PWM功能
+
+	pwm0_pin:pwm0 {
+		rockchip,pins = <PWM0>;
+		rockchip,pull = <VALUE_PULL_DISABLE>;
+		rockchip,drive = <VALUE_DRV_DEFAULT>;
+	};
+
+	pwm0: pwm@ff680000 {
+		compatible = "rockchip,rk-pwm";
+		reg = <0xff680000 0x10>;
+
+		/* used by driver on remotectl'pwm */
+		interrupts = <GIC_SPI 78 IRQ_TYPE_LEVEL_HIGH>;
+		#pwm-cells = <2>;
+		pinctrl-names = "default";
+		pinctrl-0 = <&pwm0_pin>;
+		clocks = <&clk_gates11 11>;
+		clock-names = "pclk_pwm";
+		status = "disabled";
+	};
+
+# Usage
+
+在主dts文件中包含pwm的dtsi文件
+
+	#include "pwm_backlight.dtsi"
+
 # 测试方法
 
 ### 每一秒钟修改一次背光
--- debug/pwm/pwm_backlight.c
+++ debug/pwm/pwm_backlight.c
@@ -12,30 +12,6 @@
 #include <linux/slab.h>
 #include <linux/delay.h>
 
-#if 0
-backlight {
-	compatible = "pwm-backlight";
-	pwms = <&pwm0 0 25000>;
-	brightness-levels = <255 254 253 252 251 250 249 248 247 246 245 244 243 242 241 240
-		 239 238 237 236 235 234 233 232 231 230 229 228 227 226 225 224 223 222 221 220
-		 219 218 217 216 215 214 213 212 211 210 209 208 207 206 205 204 203 202 201 200
-		 199 198 197 196 195 194 193 192 191 190 189 188 187 186 185 184 183 182 181 180
-		 179 178 177 176 175 174 173 172 171 170 169 168 167 166 165 164 163 162 161 160
-		 159 158 157 156 155 154 153 152 151 150 149 148 147 146 145 144 143 142 141 140
-		 139 138 137 136 135 134 133 132 131 130 129 128 127 126 125 124 123 122 121 120
-		 119 118 117 116 115 114 113 112 111 110 109 108 107 106 105 104 103 102 101 100
-		 99 98 97 96 95 94 93 92 91 90 89 88 87 86 85 84 83 82 81 80 79 78 77 76 75 74 73 72 71 70
-		 69 68 67 66 65 64 63 62 61 60 59 58 57 56 55 54 53 52 51 50 49 48 47 46 45 44 43 42 41 40
-		 39 38 37 36 35 34 33 32 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10
-		 9 8 7 6 5 4 3 2 1 0>;
-	default-brightness-level = <200>;
-	enable-gpios = <&gpio7 GPIO_A2 GPIO_ACTIVE_HIGH>;
-			bp_power = <&gpio7 GPIO_B0 GPIO_ACTIVE_HIGH>;
-			bp_reset = <&gpio7 GPIO_C5 GPIO_ACTIVE_HIGH>;
-			bp_wakeup_ap = <&gpio7 GPIO_C0 GPIO_ACTIVE_HIGH>;
-};
-#endif
-
 struct pwm_bl_data {
 	struct pwm_device	*pwm;
 	struct device		*dev;
@@ -181,9 +157,6 @@ static int pwm_backlight_parse_dt(struct device *dev,
 	if (gpio_is_valid(data->enable_gpio) && (flags & OF_GPIO_ACTIVE_LOW))
 		data->enable_gpio_flags |= PWM_BACKLIGHT_GPIO_ACTIVE_LOW;
 
-	data->bp_power = of_get_named_gpio_flags(node, "bp_power", 0, &flags);
-	data->bp_reset = of_get_named_gpio_flags(node, "bp_reset", 0, &flags);
-
 	return 0;
 }
 
@@ -254,31 +227,6 @@ static int pwm_backlight_probe(struct platform_device *pdev)
 			dev_err(&pdev->dev, "failed to request GPIO#%d: %d\n", pbd->enable_gpio, ret);
 			goto err_alloc;
 		}
-	}
-
-	printk("bp_power = %d, bp_reset = %d\n", data->bp_power, data->bp_reset);
-	if (gpio_is_valid(data->bp_power)) {
-		ret = devm_gpio_request(&pdev->dev, data->bp_power, NULL);
-		if (ret < 0) {
-			dev_err(&pdev->dev, "failed to request GPIO#%d: %d\n", data->bp_power, ret);
-			goto err_alloc;
-		}
-
-		gpio_direction_output(data->bp_power, 1);
-	}
-
-	if (gpio_is_valid(data->bp_reset)) {
-		ret = devm_gpio_request(&pdev->dev, data->bp_reset, NULL);
- 		if (ret < 0) {
-			dev_err(&pdev->dev, "failed to request GPIO#%d: %d\n", data->bp_reset, ret);
-			goto err_alloc;
-		}
-
-		/* do a reset  */
-		gpio_direction_output(data->bp_reset, 1);
-   		gpio_set_value(data->bp_reset, 0);
-		msleep(100);
-		gpio_set_value(data->bp_reset, 1);
 	}
 
 	/*
