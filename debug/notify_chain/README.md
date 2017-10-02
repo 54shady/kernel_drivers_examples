acaba12f444ca950cea9625d5d6dbec1607dce9e
zeroway <M_O_Bz@163.com>
2017-10-02 22:45:53 +0000

    master clean

--- debug/notify_chain/README.md
+++
@@ -1,5 +0,0 @@
-# Notify
-
-[参考文章](http://blog.chinaunix.net/uid-25871104-id-3086446.html)
-
-![call flow](./call_flow.png)
--- debug/test_notify_chain/test_chain.h
+++
@@ -1,6 +0,0 @@
-#include <linux/notifier.h>
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#define TESTCHAIN_EVENT 0x1234
