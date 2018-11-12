7b6f2d7c880fbc2ba4771235c6e923e6b59f0fc2
zeroway <M_O_Bz@163.com>
2018-11-12 14:34:45 +0800

    Uart : add a loop test application

---
+++ debug/uart/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+
+# uart_loop
+include $(CLEAR_VARS)
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES := uart_loop_test.c
+LOCAL_MODULE := uart_loop
+
+include $(BUILD_EXECUTABLE)
