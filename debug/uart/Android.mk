6149ee12bc89ee70094dcd9f10f6454099bb794b
zeroway <M_O_Bz@163.com>
2018-11-12 17:08:20 +0800

    Uart : multiply I/O module (select) version

--- debug/uart/Android.mk
+++ debug/uart/Android.mk
@@ -9,3 +9,13 @@ LOCAL_SRC_FILES := uart_loop_test.c
 LOCAL_MODULE := uart_loop
 
 include $(BUILD_EXECUTABLE)
+
+# record read write
+include $(CLEAR_VARS)
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES := uart_record_rw.c
+LOCAL_MODULE := uart_record_rw
+
+include $(BUILD_EXECUTABLE)
