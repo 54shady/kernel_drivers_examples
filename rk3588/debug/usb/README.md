67ea220f69b15d2cb017f3c95b7175b791bcbf45
zeroway <M_O_Bz@163.com>
2024-06-03 13:57:34 +0800

    RK3588 usb ffs test demo

---
+++ rk3588/debug/usb/README.md
@@ -0,0 +1,87 @@
+# USB FFS test demo
+
+## 编译Host App
+
+编译host app在主机上运行
+
+	cd ffs-aio-example/host_app/
+	make
+
+## 编译Device App
+
+使用edge SDK自带的交叉编译工具链
+
+	export ARCH=arm64
+	export CROSS_COMPILE=~/src/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
+交叉编译libaio(commit b8eadc9)和device app
+
+下载libaio代码
+
+	git clone https://pagure.io/libaio.git
+
+修改src/Makefile添加如下(使用交叉编译工具链)
+
+	CC = $(CROSS_COMPILE)gcc
+	AR = $(CROSS_COMPILE)ar
+
+	make
+
+编译生成动态和静态库
+
+	src/libaio.a
+	src/libaio.so.1.0.2
+
+将静态库和头文件拷贝到指定目录
+
+	cp src/libaio.a ffs-aio-example/device_app/
+	cp src/libaio.h ffs-aio-example/device_app/
+
+编译device app
+
+	cd ffs-aio-example/device_app/
+	make
+
+## 测试
+
+进入到设备端运行usb.sh进行配置
+
+	usb.sh
+
+在设备端运行device app
+
+	./aio_multibuff /dev/usb-ffs/test
+
+	echo fc000000.usb > /config/usb_gadget/g1/UDC
+	echo device > /sys/kernel/debug/usb/fc000000.usb/mode
+
+其中fc000000对应的是dwc3(arch/arm64/boot/dts/rockchip/rk3588s.dtsi)
+
+	usbdrd_dwc3_0: usb@fc000000 {
+		compatible = "snps,dwc3";
+
+此时能在主机lsusb上看到如下设备
+
+	Bus 003 Device 087: ID 1d6b:0105 Linux Foundation FunctionFS Gadget
+
+查看设备信息
+
+	lsusb -v -d 1d6b:0105
+
+	cat /sys/kernel/debug/usb/devices
+	...
+	T:  Bus=03 Lev=01 Prnt=01 Port=02 Cnt=02 Dev#= 87 Spd=480  MxCh= 0
+	D:  Ver= 2.10 Cls=00(>ifc ) Sub=00 Prot=00 MxPS=64 #Cfgs=  1
+	P:  Vendor=1d6b ProdID=0105 Rev= 3.10
+	S:  Manufacturer=myusb
+	S:  Product=myusbtest
+	S:  SerialNumber=0123456789
+	C:* #Ifs= 1 Cfg#= 1 Atr=80 MxPwr=500mA
+	I:* If#= 0 Alt= 0 #EPs= 2 Cls=ff(vend.) Sub=00 Prot=00 Driver=(none)
+	E:  Ad=81(I) Atr=02(Bulk) MxPS= 512 Ivl=0ms
+	E:  Ad=01(O) Atr=02(Bulk) MxPS= 512 Ivl=0ms
+	...
+
+在主机上运行host app
+
+	sudo ffs-aio-example/host_app/multibuff_test
---
+++ rk3588/debug/usb/usb.sh
@@ -0,0 +1,59 @@
+# unplug usb cable (option) or just keep usb plugin when bootup
+
+#mkdir /config
+mount -t configfs none /config
+
+mkdir /config/usb_gadget/g1/
+
+# 使用Linux FunctionFS Gadget 的vendor id 和product id
+# https://devicehunt.com/view/type/usb/vendor/1D6B
+echo 0x1d6b > /config/usb_gadget/g1/idVendor
+echo 0x0105 > /config/usb_gadget/g1/idProduct
+echo 0x0310 > /config/usb_gadget/g1/bcdDevice
+echo 0x0200 > /config/usb_gadget/g1/bcdUSB
+
+mkdir -p /config/usb_gadget/g1/strings/0x409
+echo 0123456789 > /config/usb_gadget/g1/strings/0x409/serialnumber
+
+echo "myusb" > /config/usb_gadget/g1/strings/0x409/manufacturer
+echo "myusbtest" > /config/usb_gadget/g1/strings/0x409/product
+
+mkdir /config/usb_gadget/g1/configs/b.1
+mkdir /config/usb_gadget/g1/configs/b.1/strings/0x409
+echo "test" > /config/usb_gadget/g1/configs/b.1/strings/0x409/configuration
+echo 500 > /config/usb_gadget/g1/configs/b.1/MaxPower
+
+echo 0x1 > /config/usb_gadget/g1/os_desc/b_vendor_code
+echo "MSFT100" > /config/usb_gadget/g1/os_desc/qw_sign
+ln -s /config/usb_gadget/g1/configs/b.1 /config/usb_gadget/g1/os_desc/b.1
+
+mkdir /config/usb_gadget/g1/functions/ffs.test
+ln -s /config/usb_gadget/g1/functions/ffs.test /config/usb_gadget/g1/configs/b.1/f1
+
+mkdir -p /dev/usb-ffs/test
+mount -o rmode=0770,fmode=0660,uid=1024,gid=1024 -t functionfs test /dev/usb-ffs/test
+
+# 在新窗口中手动执行
+#./aio_multibuff /dev/usb-ffs/test
+
+# plug usb cable (option)
+
+# connect usb
+#echo fc000000.usb > /config/usb_gadget/g1/UDC
+#echo device > /sys/kernel/debug/usb/fc000000.usb/mode
+
+# 串口中会看到如下日志
+#[  507.567791] android_work: sent uevent USB_STATE=CONNECTED
+#[  507.572767] android_work: sent uevent USB_STATE=CONFIGURED
+
+#此时在主机上可以通过lsusb看到枚举的设备
+#Bus 003 Device 042: ID 1d6b:0105 Linux Foundation FunctionFS Gadget
+# 在主机上执行测试
+#sudo ffs-aio-example/host_app/multibuff_test
+
+# disconnect usb
+#echo  > /sys/kernel/debug/usb/fc000000.usb/mode
+#echo > /config/usb_gadget/g1/UDC
+
+# 串口中会看到如下日志
+#[  701.327453] android_work: sent uevent USB_STATE=DISCONNECTED
