283a445958d38f5e6a2441e9617584b7d20248d8
zeroway <M_O_Bz@163.com>
2024-06-03 15:47:40 +0800

    Bind config with the same name under device node

--- rk3588/debug/usb/usb.sh
+++ rk3588/debug/usb/usb.sh
@@ -20,21 +20,22 @@ echo "myusbtest" > /config/usb_gadget/g1/strings/0x409/product
 
 mkdir /config/usb_gadget/g1/configs/b.1
 mkdir /config/usb_gadget/g1/configs/b.1/strings/0x409
-echo "test" > /config/usb_gadget/g1/configs/b.1/strings/0x409/configuration
+echo "mytest" > /config/usb_gadget/g1/configs/b.1/strings/0x409/configuration
 echo 500 > /config/usb_gadget/g1/configs/b.1/MaxPower
 
 echo 0x1 > /config/usb_gadget/g1/os_desc/b_vendor_code
 echo "MSFT100" > /config/usb_gadget/g1/os_desc/qw_sign
 ln -s /config/usb_gadget/g1/configs/b.1 /config/usb_gadget/g1/os_desc/b.1
 
-mkdir /config/usb_gadget/g1/functions/ffs.test
-ln -s /config/usb_gadget/g1/functions/ffs.test /config/usb_gadget/g1/configs/b.1/f1
+# 通过名字mydemo来绑定ffs和/dev/usb-ffs/对应的目录
+mkdir /config/usb_gadget/g1/functions/ffs.mydemo
+ln -s /config/usb_gadget/g1/functions/ffs.mydemo /config/usb_gadget/g1/configs/b.1/f1
 
-mkdir -p /dev/usb-ffs/test
-mount -o rmode=0770,fmode=0660,uid=1024,gid=1024 -t functionfs test /dev/usb-ffs/test
+mkdir -p /dev/usb-ffs/mydemo
+mount -o rmode=0770,fmode=0660,uid=1024,gid=1024 -t functionfs mydemo /dev/usb-ffs/mydemo
 
 # 在新窗口中手动执行
-#./aio_multibuff /dev/usb-ffs/test
+#./aio_multibuff /dev/usb-ffs/mydemo
 
 # plug usb cable (option)
 
